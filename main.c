9e8d291cdd96c8c1fe49e1c7c322c952c7992cea
pamodith-rathnaweera <as20240536@sci.sjp.ac.lk>
2025-10-26 14:22:01 +0530

    Added Vehicle information and defined global variables

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
- int main()
- {
-     printf("Hello world");
-     return 0;
- }
+
+#define MAX_CITIES 30
+#define MAX_DELIVERIES 50
+#define FUEL_PRICE 310.0
+
+
+char *vehicle[3] = {"Van", "Truck", "Lorry"};
+int capacity[3] = {1000, 5000, 10000};
+float ratePerKm[3] = {30, 40, 80};
+float avgSpeed[3] = {60, 50, 45};
+float fuelEfficiency[3] = {12, 6, 4};
