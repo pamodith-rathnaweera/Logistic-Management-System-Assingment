ee7009e2700dd814d54b5c3ddb3264435d465faa
pamodith-rathnaweera <as20240536@sci.sjp.ac.lk>
2025-10-26 14:25:43 +0530

    Added suitable arrays for city data and  delivery records

--- main.c
+++ main.c
@@ -11,3 +11,19 @@ int capacity[3] = {1000, 5000, 10000};
 float ratePerKm[3] = {30, 40, 80};
 float avgSpeed[3] = {60, 50, 45};
 float fuelEfficiency[3] = {12, 6, 4};
+
+char cities[MAX_CITIES][30];
+int cityCount = 0;
+float distance[MAX_CITIES][MAX_CITIES];
+
+int deliverySource[MAX_DELIVERIES];
+int deliveryDestination[MAX_DELIVERIES];
+float deliveryWeight[MAX_DELIVERIES];
+int deliveryVehicleType[MAX_DELIVERIES];
+float deliveryCost[MAX_DELIVERIES];
+float fuelUsedList[MAX_DELIVERIES];
+float totalCostList[MAX_DELIVERIES];
+float profitList[MAX_DELIVERIES];
+float chargeList[MAX_DELIVERIES];
+float timeList[MAX_DELIVERIES];
+int deliveryCount= 0;
